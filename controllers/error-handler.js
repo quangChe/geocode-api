4b46f756b3b6400620e6b74ae8945773c2637697
quangChe <quang.n.che@gmail.com>
2019-08-01 23:05:16 -0700

    Error Handling
    Added to error-handling middleware a series of response messages for
    handling errors from app

--- controllers/error-handler.js
+++ controllers/error-handler.js
@@ -1,5 +1,17 @@
+const errorResponse = {
+  ADDRESS_COMPONENTS_QUERY: (res) => 
+    res.status(400).send('Request must contain an "address" or "components" query parameter'),
+  GEOCODE_QUERY: (res) =>
+    res.status(400).send('Query must contain a "geocode" query parameter'),
+  INVALID_GEOCODE_VALUE: (res) =>
+    res.status(400).send('Invalid latitude and longitude value. Use "lat,long" or "long,lat"')
+};
+
 const errorHandler = (error, req, res, next) => {
-  res.status(500).send(error.message);
+  if (!errorResponse[error.message]) {
+    return res.status(500).send(error.message || 'Unexpected error!');
+  }
+  return errorResponse[error.message](res);
 }
 
 module.exports = errorHandler;
