16846ecae1fcc2b1f5a8879c99369ea3996230de
quangChe <quang.n.che@gmail.com>
2019-08-01 22:17:36 -0700

    Installed axios@0.18.1 (more stable) and added a 'getGeocode' request to Google API

--- controllers/google-api.js
+++ controllers/google-api.js
@@ -1,12 +1,31 @@
+const axios = require('axios');
+axios.defaults.baseURL = 'https://maps.googleapis.com/maps/api/';
+axios.defaults.params = { key: process.env.GOOGLE_API_KEY };
+
 module.exports = {
-  getGeoCode: (req, res, next) => {
-    res.status(200).send('GET CODE');
+  getGeocode: async (req, res) => {
+    try {
+      const response = await axios.get('/geocode/json', {
+        params: { 
+          address: req.query.address, 
+          components: req.query.components
+        }
+      });
+      const geocodeData = response.data.results.map(data => ({
+        name: data.formatted_address,
+        geometry: data.geometry.location
+      }));
+
+      res.status(200).send(geocodeData);
+    } catch(err) {
+      console.error(err);
+    }   
   },
 
-  reverseGeoCode: (req, res, next) => {
-    res.status(200).send('REVERSE');
+  reverseGeocode: (req, res, next) => {
+    // const 
   },
-  getDistance: (req, res, next) => {
+  geocodesDistance: (req, res, next) => {
     res.status(200).send('DISTANCE');
   }
 }
