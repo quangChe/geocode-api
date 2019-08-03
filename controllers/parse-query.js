2fd7027bbe0ee334747737c35f7c4ebeba31da43
quangChe <quang.n.che@gmail.com>
2019-08-02 21:53:46 -0700

    Pasing query for two coordinates

--- controllers/parse-query.js
+++ controllers/parse-query.js
@@ -5,24 +5,40 @@ const parse = {
     const { address, components } = req.query;
 
     if (!address && !components) {
-      return next(new Error('ADDRESS_COMPONENTS_QUERY'));
+      return res.status(400).send('Request needs a "address" or "components" query parameter');
     }
 
     return next();
   },
   geocodeQuery: (req, res, next) => {
     const { geocode } = req.query;
-    let err = null;
 
     if (!geocode) {
-      err = new Error('GEOCODE_QUERY');
+      return res.status(400).send('Request needs a "geocode" query parameter');
     } else if (!validate.isLatLong(geocode)) {
-      err = new Error('INVALID_GEOCODE_VALUE');
+      return res.status(400).send('Geocode value must be "lat,long" or "long,lat"');
     }
     
-    return err ? next(err) : next();
+    return next();
   },
-  
+  distanceQuery: (req, res, next) => {
+    const { p1, p2 } = req.query;
+
+    const validLatLng = (lat, lng) => validate.isLatLong(`${lat},${lng}`);
+
+    if (!p1 || !p2) {
+      return res.status(400).send('Request needs "p1" and "p2" query parameters');
+    } else if (!p1.lat || !p1.lng || !p2.lat || !p2.lng) {
+      console.log(p1.lat, p2.lat, p1.lng, p2.lng);
+      return res.status(400).send('"p1" and "p2" must have format {lat: "", lng; ""}')
+    } else if (p1.lat === p2.lat && p1.long === p2.long) {
+      return res.status(400).send('"p1" and "p2" have the same latitude and longitude');
+    } else if (!validLatLng(p1.lat, p1.lng) || !validLatLng(p2.lat, p2.lng)) {
+      return res.status(400).send('Geocode are invalid');
+    }
+
+    return next();
+  }
 }
 
 module.exports = parse;
